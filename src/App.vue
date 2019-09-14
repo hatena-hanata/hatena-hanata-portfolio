b17be18294389558e8a7ceffed1e437450a22b7d
haten-hanata <hanatasfp@gmail.com>
2019-09-14 21:08:29 +0900

    ヘッダー完成

--- src/App.vue
+++ src/App.vue
@@ -15,16 +15,23 @@
     <v-content>
       
       <div class='pc'>
+        <div class="img-header-pc">
+          <img src='@/assets/header.png' class='sp-card-image pt-4'/>
+          <p>Portfolio</p>
+      </div>
         <HelloWorld/>
       </div>
 
       <div class='sp'>
+        <div class="img-header-sp">
+          <img src='@/assets/header.png' class='sp-card-image pt-4'/>
+          <p>Portfolio</p>
+      </div>
         <SpProfile/>
       </div>
       
     </v-content>
   </v-app>
-  <!-- <img src="@/assets/logo.png" class="logo"/> -->
 </template>
 
 <script>
@@ -50,4 +57,50 @@ export default {
 .pc { display: none !important; }
 .sp { display: block !important; }
 }
+
+.img-header-pc {
+  position: relative;
+  
+}
+.img-header-pc img {
+  opacity: 0.2;
+}
+
+.img-header-pc p {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translate(-50%,-50%);
+  -webkit-transform: translate(-50%,-50%);
+  transform: translate(-50%,-50%);
+  margin:0;
+  padding:0;
+  /* font-weight: bold; 太字に */
+  font-size: 5em;/*サイズ2倍*/
+  font-family :Roboto, sans-serif;/*Google Font*/
+  /*文字の装飾は省略*/
+  }
+
+  .img-header-sp {
+  position: relative;
+  
+}
+.img-header-sp img {
+  opacity: 0.2;
+}
+
+.img-header-sp p {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translate(-50%,-50%);
+  -webkit-transform: translate(-50%,-50%);
+  transform: translate(-50%,-50%);
+  margin:0;
+  padding:0;
+  /* font-weight: bold; 太字に */
+  font-size: 2em;/*サイズ2倍*/
+  font-family :Roboto, sans-serif;/*Google Font*/
+  /*文字の装飾は省略*/
+  }
 </style>
